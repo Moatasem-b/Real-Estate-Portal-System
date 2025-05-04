e55eb7ca41272f9b9e0841d05da88d14ef13666a
Mohamed Saad <you@example.com>
2025-05-04 15:22:49 +0300

    Add file handling for users

--- User.h
+++ User.h
@@ -8,24 +8,26 @@ private:
     int id;
     string username;
     string password;
-    bool admin;
+    int admin;
 public:
-    User(int id, const string& username, const string& password, bool admin = false);
+    User(int id, const string& username, const string& password, int admin = 0);
 
     void setUsername(const string& user);
     void setPassword(const string& pass);
 
+    int getId();
     string getUsername() const;
     string getPassword() const;
-    bool isAdmin() const;
+    int isAdmin() const;
 };
 
-User::User(int id, const string& username, const string& password, bool admin)
+User::User(int id, const string& username, const string& password, int admin)
     : id(id), username(username), password(password), admin(admin) {}
 
 void User::setUsername(const string& user) { username = user;}
 void User::setPassword(const string& pass) { password = pass; }
 
+int User::getId() { return id; }
 string User::getUsername() const { return username; }
 string User::getPassword() const { return password; }
-bool User::isAdmin() const { return admin; }
+int User::isAdmin() const { return admin; }
